c34087456ecdad7c3ee4a735389339d1d9142a0b
Felipe Almeida <felipe.lipe12.fl@outlook.com>
2023-05-15 16:44:56 -0300

    Sidebar (Desktop) Finish

--- src/libs/components/global/Sidebar/index.tsx
+++ src/libs/components/global/Sidebar/index.tsx
@@ -1,17 +1,14 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, IconButton, Image } from "@chakra-ui/react";
 import { Information } from "../../widgets/Cards/Information";
 import { Navigation } from "./Navigation";
 
 export const Sidebar = () => {
     return (
-        <Flex direction='column' gap='4' bg='winter.100' w='full' p='4' rounded='2xl' overflow='hidden'>
+        <Flex direction='column' gap='4' bg='winter.700' w='full' p='4' rounded='2xl' overflow='hidden'>
             <Flex direction='column' gap='4' h='full' overflow='auto'>
                 <Flex justify="center">
-                    <Flex gap='4'>
-                        <Image src='/ChakraUI.svg' w='100px' />
-                    </Flex>
-                    <Flex>
-                        {/* Toggle */}
+                    <Flex align='center' gap='4' py='2'>
+                        <Image src='/ChakraUI.svg' w='150px' />
                     </Flex>
                 </Flex>
 
